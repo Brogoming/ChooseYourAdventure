4fb052a4c9714222f6f785ece20b5bedb8d98878
kotag <kotag@KotaLaptop>
2022-07-30 23:10:22 -0500

    I made a basic turn base combat with options. I don't know if I'm thinking too hard or not. I'm "okay" with the combat, it's good enough know so I can make a simple little adventure and tell a simple story. there is a lot more that should be added to make it feel more like an RPG but I don't know where my goals are at right now.

--- CYAdventure/src/Events.java
+++ CYAdventure/src/Events.java
@@ -9,40 +9,48 @@ public class Events {
 	
 	public Events() {
 		Classes();
-		PlayerSetup();
+		PlayerCharacter();
 		StartUp();
 	}
 	
 	public void Classes() { //loads all of the excess classes
 		input = new Input();
 		player = new Player();
-		goblin = new Enemy("Goblin", 5, 5, 5, 1, "easy");
+		goblin = new Enemy("Goblin", 7, 5, 10, 5, "easy");
 	}
 	
-	public void PlayerSetup() { //sets up the player stats
-		System.out.println("What is your name?");
-		player.setName(input.stringInput());
-		System.out.println("How much health do you have?");
-		player.setHealth(input.numInput());
-		System.out.println("How much speed do you have?");
-		player.setSpeed(input.numInput());
-		System.out.println("How much damage do you have?");
-		player.setDamage(input.numInput());
-		System.out.println("How much defence do you have?");
-		player.setDefence(input.numInput());
-	} //end of player stats
+	public void PlayerCharacter() { //basic classes to start with
+		System.out.println("========================================");
+		System.out.println("What would you like to play as?");
+		System.out.println("1. Knight");
+		System.out.println("2. Rogue");
+		System.out.println("3. Wizard");
+		System.out.println("----------------------------------------");
+		int answer = input.numInput();
+		switch (answer) {
+		case 1: 
+			player = new Player("Knight", 15, 5, 10, "Strike", "Shoulder Bump", "Impale", 8, 4, 7);
+			break;
+		case 2:
+			player = new Player("Rogue", 12, 15, 3, "Stab", "Trip", "Slice", 5, 2, 6);
+			break;
+		case 3:
+			player = new Player("Wizard", 8, 12, 10, "Fire Ball", "Lightening Strike", "Extra Hands", 8, 9, 6);
+			break;
+		}
+	}
 	
-	public void Playerstats() { //shows that players stats
+	public void Playerstats(Player player) { //shows that players stats
 		System.out.println("========================================");
 		System.out.println("Player: " + player.getName());
-		System.out.println("Health: " + player.getHealth() + " Speed: " + player.getSpeed() + " Damage: " + player.getDamage() + " Defence: " + player.getDefence());
+		System.out.println("Health: " + player.getHealth() + " Speed: " + player.getSpeed() + " Defence: " + player.getDefence());
 		System.out.println("========================================");
 	} //end of player stats
 	
 	public void Enemystats(Enemy enemy) { //shows that enemy's stats
 		System.out.println("========================================");
 		System.out.println("Enemy: " + enemy.getName());
-		System.out.println("Health: " + enemy.getHealth() + " Speed: " + enemy.getSpeed() + " Damage: " + enemy.getDamage() + " Defence: " + enemy.getDefence());
+		System.out.println("Health: " + enemy.getHealth() + " Speed: " + enemy.getSpeed() + " Defence: " + enemy.getDefence());
 		System.out.println("========================================");
 	} //end of player enemy's
 	
@@ -55,10 +63,26 @@ public class Events {
 		while(attacking){
 			if (enemy.getSpeed() > player.getSpeed()) { //the enemy attacks
 				EnemyTurn(player, enemy);
+				if (TestDead(player, enemy)) {
+					attacking = false; 
+					continue;
+				}
 				PlayerTurn(player, enemy);
+				if (TestDead(player, enemy)) {
+					attacking = false; 
+					continue;
+				}
 			} else {
 				PlayerTurn(player, enemy);
+				if (TestDead(player, enemy)) {
+					attacking = false; 
+					continue;
+				}
 				EnemyTurn(player, enemy);
+				if (TestDead(player, enemy)) {
+					attacking = false; 
+					continue;
+				}
 			}
 		}
 	} //end of combat
@@ -71,27 +95,25 @@ public class Events {
 			if(enemy.getHealth() <= 0) {
 				System.out.println(enemy.getName() + " is dead!");
 			}
-			return false;
-		} else {
 			return true;
+		} else {
+			return false;
 		}
 	}
 	
 	private void PlayerTurn(Player player, Enemy enemy) { //players turn during combat
-		Playerstats();
+		Playerstats(player);
 		System.out.println(player.getName() + "'s Turn");
-		System.out.println("Enter 1 to continue");
-		if (input.isStringInt(input.stringInput())) {
-			System.out.println("----------------------------------------");
+		System.out.println("----------------------------------------");
+			player.Moves(input, player.getM1(), player.getM2(), player.getM3(), player.getD1(), player.getD2(), player.getD3());
 			if (random.nextInt(20) + 1 >= enemy.getSpeed()) {
 				int damage = Damage(enemy.getDefence(), player.getDamage());
-				System.out.println(enemy.getName() + " is delt " + damage + "!");
+				System.out.println(enemy.getName() + " is delt " + damage + " damage!");
 				enemy.setHealth(enemy.getHealth() - damage);
 			} else {
-				System.out.println("Miss!");
+			System.out.println("Miss!");
 			}
-			return;
-		}
+		return;
 	} //end of player turn
 
 	private void EnemyTurn(Player player, Enemy enemy) { //enemy's turn during combat
@@ -100,7 +122,7 @@ public class Events {
 		System.out.println("----------------------------------------");
 		if (random.nextInt(20) + 1 >= player.getSpeed()) {
 			int damage = Damage(player.getDefence(), enemy.getDamage());
-			System.out.println("Player is delt " + damage + "!");
+			System.out.println(player.getName() + " is delt " + damage + " damage!");
 			player.setHealth(player.getHealth() - damage);
 		} else {
 			System.out.println("Miss!");
@@ -109,12 +131,17 @@ public class Events {
 	} //end of enemy turn
 
 	private int Damage(int defence, int attack) { //damage inflicted during combat
-		int damage = attack - defence;
-		if (damage > 0) {
-			return damage;
-		} else {
+		if(random.nextInt(20) + 1 < defence) {
 			System.out.println("No damage!");
 			return 0;
+		} else {
+			int damage = attack - defence;
+			if (damage > 0) {
+				return damage;
+			} else {
+				System.out.println("No damage!");
+				return 0;
+			}
 		}
 	} //end of damage
 }
--- CYAdventure/src/Player.java
+++ CYAdventure/src/Player.java
@@ -11,6 +11,12 @@ public class Player {
 	private int neededXp;
 	//Inventory
 	private int money;
+	private String M1;
+	private String M2;
+	private String M3;
+	private int D1;
+	private int D2;
+	private int D3;
 	
 	public Player() { //default constructor
 		this.name = null;
@@ -24,17 +30,90 @@ public class Player {
 		this.neededXp = 100 * level;
 	}
 	
-	public Player(String name, int health, int damage, int speed, int defence) {
+	public Player(String name, int health, int speed, int defence, String M1, String M2, String M3, int D1, int D2, int D3) {
 		this.name = name;
 		this.health = health;
-		this.damage = damage;
 		this.speed = speed;
 		this.defence = defence;
+		this.M1 = M1;
+		this.M2 = M2;
+		this.M3 = M3;
+		this.D1 = D1;
+		this.D2 = D2;
+		this.D3 = D3;
+		
 	}
 	
 	public void LevelUp() { //levels up the player
 		if (currentXp == neededXp) {
 			setLevel(+1);
+		}
+	}
+	
+	public String getM1() {
+		return M1;
+	}
+
+	public void setM1(String m1) {
+		M1 = m1;
+	}
+
+	public String getM2() {
+		return M2;
+	}
+
+	public void setM2(String m2) {
+		M2 = m2;
+	}
+
+	public String getM3() {
+		return M3;
+	}
+
+	public void setM3(String m3) {
+		M3 = m3;
+	}
+
+	public int getD1() {
+		return D1;
+	}
+
+	public void setD1(int d1) {
+		D1 = d1;
+	}
+
+	public int getD2() {
+		return D2;
+	}
+
+	public void setD2(int d2) {
+		D2 = d2;
+	}
+
+	public int getD3() {
+		return D3;
+	}
+
+	public void setD3(int d3) {
+		D3 = d3;
+	}
+
+	public void Moves(Input input, String M1, String M2, String M3, int D1, int D2, int D3) {
+		System.out.println("1. " + M1);
+		System.out.println("2. " + M2);
+		System.out.println("3. " + M3);
+		System.out.println("----------------------------------------");
+		int answer = input.numInput();
+		switch(answer) {
+		case 1: 
+			setDamage(D1);
+			break;
+		case 2: 
+			setDamage(D2);
+			break;
+		case 3: 
+			setDamage(D3);
+			break;
 		}
 	}
 
